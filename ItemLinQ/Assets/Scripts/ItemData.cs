db50ef940718e1a857499906a37df0377c06afbc
KID <50666207+KID421@users.noreply.github.com>
2020-07-14 15:17:44 +0800

    網頁版

---
+++ ItemLinQ/Assets/Scripts/ItemData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public enum ItemType { weapon, prop };
+
+[CreateAssetMenu(menuName = "KID/道具資料", fileName = "道具資料")]
+public class ItemData : ScriptableObject
+{
+    public string _name;
+    public Sprite sprite;
+    public ItemType itemType;
+
+    public bool A;
+    public bool B;
+}
---
+++ ItemLinQ/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections.Generic;
+
+public class ItemManager : MonoBehaviour
+{
+    public List<ItemData> items = new List<ItemData>();
+
+    public void TypeOfProp()
+    {
+        var result = items.Where((x) => x.itemType == ItemType.prop);
+
+        CreateItem(result);
+    }
+
+    private void CreateItem(IEnumerable<ItemData> result)
+    {
+        for (int i = 0; i < transform.childCount; i++) Destroy(transform.GetChild(i).gameObject);
+
+        foreach (var item in result)
+        {
+            GameObject temp = Instantiate(new GameObject(), transform);
+            temp.AddComponent<Image>().sprite = item.sprite;
+        }
+    }
+}
